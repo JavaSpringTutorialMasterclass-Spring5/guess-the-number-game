814470b8e8c6e3489cc13d7cbc829f7f51c2f9d6
Bartłomiej Mytko <bartlomiej.mytko@komputronik.pl>
2019-07-30 12:50:17 +0200

    Create context container

--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -2,6 +2,8 @@ package academy.learnprogramming;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
@@ -11,5 +13,8 @@ public class Main {
 
     public static void main(String[] args) {
         log.info("Guess The Number Game");
+
+        //create context container
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
     }
 }
