4d6666325f9bbb603bb1b378ad063a2a2c7692b9
Bartłomiej Mytko <bartlomiej.mytko@komputronik.pl>
2019-08-08 15:17:29 +0200

    Dodanie klasy AppConfig (oadnotowanej @Configuration) z dwoma metodami (oadnotowanymi @Bean) produkującymi beany

--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -3,19 +3,18 @@ package academy.learnprogramming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     private final static Logger log = LoggerFactory.getLogger(Main.class);
 
-    private static final String CONFIG_LOCATION = "beans.xml";
-
     public static void main(String[] args) {
         log.info("Guess The Number Game");
 
         //create context container
-        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
         //odpytujemy kontekst o odpowiednią instancję beana po nazwie (id z pliku beans) i typie
         NumberGenerator numberGenerator = context.getBean(NumberGenerator.class);
