7e7d87ae4e803a9c016ad9453af40ffdf363f222
Bartłomiej Mytko <bartlomiej.mytko@komputronik.pl>
2019-08-26 12:33:29 +0200

    Dodanie logiki metody getResultMessage w klasie MessageGenratorImpl

--- core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
+++ core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
@@ -17,16 +17,33 @@ public class MessageGeneratorImpl implements MessageGenerator {
 
     @PostConstruct
     public void init() {
-        log.info("Obiekt game to: " + game);
+        log.info("Obiekt game to: {}", game);
     }
 
     @Override
     public String getMainMessage() {
-        return "uruchomiono metodę getMainMessage";
+        return "Number is metween " + game.getSmallest() + " and" + game.getBiggest() + ". Can you guess it?";
     }
 
     @Override
     public String getResultMessage() {
-        return "uruchomiono metodę getResultMessage";
+
+        if (game.isGameWon()) {
+            return "You guessed it! The number was: " + game.getNumber();
+        } else if (game.isGameLost()) {
+            return "You lost! The number was: " + game.getNumber();
+        } else if (!game.isValidNumberRange()) {
+            return "Invalid number range!";
+        } else if (game.getRemainingGuesses() == guessCount) {
+            return "What is your fist guess?";
+        } else {
+            String direction = "Lower";
+
+            if (game.getGuess() < game.getNumber()) {
+                direction = "Higher";
+            }
+            return direction + "! You have " + game.getRemainingGuesses() + " guess left";
+        }
+
     }
 }
