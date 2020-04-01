c032288357031df181fa6caf6c925a92270f6860
Bartłomiej Mytko <bartlomiej.mytko@komputronik.pl>
2020-04-01 11:14:33 +0200

    Dodanie endpointu dla głównej strony i prostego szablonu thymeleaf

---
+++ web/src/main/java/academy/learnprogramming/util/ViewNames.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming.util;
+
+public final class ViewNames {
+
+    // == constans ==
+    public static final String HOME = "home";
+
+    // == constructor ==
+    private ViewNames() {
+    }
+}
