4a7d45c081cd80b8f56ffa680014c69103f7e410
Bartłomiej Mytko <bartlomiej.mytko@komputronik.pl>
2020-04-07 21:51:08 +0200

    Wprowadzenie obsługi wielu języków - w napisach pochodzących z klas javowych i przekazywnych do szablonó thymeleafowych w kontrolerach za pomocą model.addAttribute()

--- core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
+++ core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
@@ -2,6 +2,8 @@ package academy.learnprogramming;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -10,21 +12,32 @@ import javax.annotation.PostConstruct;
 @Component
 public class MessageGeneratorImpl implements MessageGenerator {
 
-    private final Game game;
+    // == constans ==
+    public static final String MAIN_MESSAGE = "game.main.message"; //klucz napisu jakiego będziemy poszukiwać w plikach messages.properties i message_es.properties
 
+    // == fields ==
+    private final Game game;
+    private final MessageSource messageSource;
+
+    // == constructor ==
     @Autowired
-    public MessageGeneratorImpl(Game game) {
+    public MessageGeneratorImpl(Game game, MessageSource messageSource) {
         this.game = game;
+        this.messageSource = messageSource;
     }
 
+    // == init ==
     @PostConstruct
     public void init() {
         log.info("Obiekt game to: {}", game);
     }
 
+
+    // == public methods ==
+
     @Override
     public String getMainMessage() {
-        return "Number is between " + game.getSmallest() + " and" + game.getBiggest() + ". Can you guess it?";
+        return getMessage(MAIN_MESSAGE, game.getSmallest(), game.getBiggest());
     }
 
     @Override
@@ -47,5 +60,10 @@ public class MessageGeneratorImpl implements MessageGenerator {
             return direction + "! You have " + game.getRemainingGuesses() + " guesses left";
         }
 
+    }
+
+    // == private methods ==
+    private String getMessage(String code, Object... args) {
+        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
     }
 }
